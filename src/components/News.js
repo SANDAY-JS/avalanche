0d45ab3d3e93e489024384896745f13533d1126b
Sun Higuchi <san.sh.s3@outlook.jp>
2021-06-09 10:22:21 +0900

    updated info

--- src/components/News.js
+++ src/components/News.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./News.css";
 
 function News() {
   return (
     <div className="news">
       <h2>~News~</h2>
-      <p>お知らせはございません。</p>
+      <div className="news__table">
+        <h3>1.新着ライブ情報</h3>
+        <p>
+          詳しくは<Link to="/information">こちら</Link>
+        </p>
+        <h3>2.AVALANCHEアカウント、始動</h3>
+        <p>
+          詳しくは<Link to="/profile">こちら</Link>
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/Info.js
+++ src/pages/Info.js
@@ -9,7 +9,12 @@ function Info() {
         <h2>Live Information</h2>
         <img src={img} alt="" />
       </figure>
-      <p>現在予定中のライブはありません。 </p>
+      <div className="info__table">
+        <h3>ライブ予定↓</h3>
+        <p>日時：7月10日(日)</p>
+        <p>場所：イオンモール近江八幡</p>
+        <small>＊詳細は後ほど掲載いたします。</small>
+      </div>
     </div>
   );
 }
