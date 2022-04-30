e70837cda38a585c3512c6e7b6051a133e861876
Sun Higuchi <san.sh.s3@outlook.jp>
2022-04-30 19:11:40 +0900

    Enabled to Show News in Top Pahe

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,35 +1,32 @@
 import React from "react";
 import Link from "next/link";
 import styles from "../styles/components/News.module.scss";
+import db from "../pages/api/db/news.json";
 
 function News() {
   return (
     <div className={styles.news}>
       <h2>News</h2>
       <div className={styles.news__container}>
-        <div className={styles.news__container__block}>
-          <Link href="https://t.co/P8Szd0Jf1h">
-            <a className={styles.news__container__block__link}>
-              【最新】AVALANCHE 1st Single「陽／さがしもの」リリーズ
-            </a>
-          </Link>
-        </div>
-
-        {/* <div className={styles.news__container__block}>
-          <Link href="/special-event">
-            <a className={styles.news__container__block__link}>
-              北近江FREAKY JAMMiN'2022 出演決定!!
-            </a>
-          </Link>
-        </div> */}
-
-        <div className={styles.news__container__block}>
-          <Link href="/profile">
-            <a className={styles.news__container__block__link}>
-              AVALANCHEアカウント、始動
-            </a>
-          </Link>
-        </div>
+        {db && db[0] ? (
+          db.map((news, i) => (
+            <div key={i} className={styles.news__container__block}>
+              {news.url ? (
+                <Link href="/profile">
+                  <a className={styles.news__container__block__link}>
+                    {news.text}
+                  </a>
+                </Link>
+              ) : (
+                <p className={styles.news__container__block__link}>
+                  {news.text}
+                </p>
+              )}
+            </div>
+          ))
+        ) : (
+          <p>現在のお知らせはございません。</p>
+        )}
       </div>
     </div>
   );
