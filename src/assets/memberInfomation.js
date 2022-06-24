866f1f67de24ca8e4673d923b66a81b3dd811e78
SANDAY-JS <san.sh.s3@outlook.jp>
2022-06-24 18:23:11 +0900

    まつしょうのDesを追加

--- src/assets/memberInfomation.js
+++ src/assets/memberInfomation.js
@@ -1,28 +1,29 @@
 const membersData = {
   Shota: {
-    name: "松田翔汰",
+    name: "松田 翔汰",
     role: "Vocal",
     twitter: "https://twitter.com/ok6969ava",
     instagram: "https://www.instagram.com/avalanche.shota/",
     // instagram2: "https://www.instagram.com/shota690/",
     image: "/images/vocal.jpg",
+    des: "2001年12月21日生まれの射手座のA型。 小中と野球に勤しみ、 ポジションはピッチャーだった。 中学生の時アコギを始めたことを機に、音楽に目覚める。 高校入学と同時にハンドボール部に入部する傍ら、学園祭への出演を目指しバンドを結成。それが後のAVALANCHEへと繋がる。 好きな食べ物はマヨネーズ。嫌いな食べ物はネギ、ピクルス。 推しは森七菜。 趣味はカラオケ、人を楽しませること、地図を見ること。 過去に「マツキンゲームズ」という名でYouTube活動を行っていた。"
   },
   Tomoki: {
-    name: "髙橋智樹",
+    name: "髙橋 智紀",
     role: "Guitar",
     twitter: "https://twitter.com/T_o_m_o_k_i_T",
     instagram: "https://www.instagram.com/takahashi_no_insta/",
     image: "/images/guitar.jpg",
   },
   Yuto: {
-    name: "川崎佑斗",
+    name: "川崎 佑斗",
     role: "Bass",
     twitter: "https://twitter.com/yt_kwsk",
     instagram: "https://www.instagram.com/unerivierecape_92/",
     image: "/images/base.jpg",
   },
   Sun: {
-    name: "樋口賛",
+    name: "樋口 賛",
     role: "Drums",
     twitter: "https://twitter.com/sanches_0819",
     instagram: "https://www.instagram.com/sanday.tsx/",
