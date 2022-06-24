171ac9463fd61f673b39b7fd5b85f17c743c1c74
SANDAY-JS <san.sh.s3@outlook.jp>
2022-06-24 20:12:50 +0900

    Adjusted sns position

--- src/components/Member.js
+++ src/components/Member.js
@@ -12,27 +12,29 @@ function Member({ member }) {
 
   return (
     <div className={styles.member}>
-      {/* Name & Role */}
-      <p className={styles.member__name__role}>
-        {member.role} : {member.name}
-      </p>
+      <div className={styles.member__infoWrapper}>
+        {/* Name & Role */}
+        <p className={styles.member__name__role}>
+          {member.role} : {member.name}
+        </p>
 
-      {/* SNS */}
-      <div className={styles.member__snsWrap}>
-        <a
-          href={member.twitter}
-          target="_blank"
-          className={styles.member__instagram}
-        >
-          <i className="fab fa-twitter" />
-        </a>
-        <a
-          href={member.instagram}
-          target="_blank"
-          className={styles.member__twitter}
-        >
-          <i className="fab fa-instagram" />
-        </a>
+        {/* SNS */}
+        <div className={styles.member__snsWrap}>
+          <a
+            href={member.twitter}
+            target="_blank"
+            className={styles.member__instagram}
+          >
+            <i className="fab fa-twitter" />
+          </a>
+          <a
+            href={member.instagram}
+            target="_blank"
+            className={styles.member__twitter}
+          >
+            <i className="fab fa-instagram" />
+          </a>
+        </div>
       </div>
 
       {/* Image */}
