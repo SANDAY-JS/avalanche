2c2c8f5ea86a0a2862cdfc79771b280468ad1f49
SANDAY-JS <san.sh.s3@outlook.jp>
2022-08-30 15:39:39 +0900

    Adjusted Font Size & design

--- src/components/LiveTable.jsx
+++ src/components/LiveTable.jsx
@@ -19,23 +19,23 @@ const LiveTable = ({event}) => {
     <table className={styles.live__table}>
       <tbody>
         <tr className={styles.live__table__content}>
-          <td>イベント名</td>
+          <td className={styles.live__table__content__menu}>イベント名</td>
           <td className={event ? event.eventName && styles.live__table__content__animTitle : styles.loading}>{!event ? '' : event.eventName ?? "未定"}</td>
         </tr>
         <tr className={styles.live__table__content}>
-          <td>日時</td>
+          <td className={styles.live__table__content__menu}>日時</td>
           <td className={event ? '' : styles.loading}>{!event ? '' : event.date ? `${adjustDate(event.date)}` : "未定"}</td>
         </tr>
         <tr className={styles.live__table__content}>
-          <td>出演時間</td>
+          <td className={styles.live__table__content__menu}>出演時間</td>
           <td className={event ? '' : styles.loading}>{!event ? '' : event.time ? `${event.time}～` : "未定"}</td>
         </tr>
         <tr className={styles.live__table__content}>
-          <td>場所</td>
+          <td className={styles.live__table__content__menu}>場所</td>
           <td className={event ? '' : styles.loading}>{!event ? '' : event.place ?? "未定"}</td>
         </tr>
         <tr className={styles.live__table__content}>
-          <td>詳細</td>
+          <td className={styles.live__table__content__menu}>詳細</td>
           {!event ? (
             <td className={styles.loading}></td>
           ) : (
