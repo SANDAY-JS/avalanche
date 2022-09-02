dfe1eb01f49988cd133ca8faaecded2f8ddc47c9
SANDAY-JS <san.sh.s3@outlook.jp>
2022-09-02 21:43:28 +0900

    Changed Top Page Component Order

--- src/components/TopPage.js
+++ src/components/TopPage.js
@@ -1,13 +1,11 @@
 import React from "react";
 import styles from "../styles/components/TopPage.module.scss";
-import News from "./News";
 import Slider from "./Slider";
 
 function TopPage() {
   return (
     <div className={styles.toppage}>
       <Slider />
-      <News />
     </div>
   );
 }
